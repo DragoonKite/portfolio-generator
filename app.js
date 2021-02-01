3737aac8808b1073e97f716a76d51724ecceb632
DragoonKite <frank.depaolo@yahoo.com>
2021-02-01 14:58:55 -0600

    added input validation to required inputs

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const { FindValueSubscriber } = require('rxjs/internal/operators/find');
 //const fs = require('fs');
 //const generatePage = require('./src/page-template.js');
 
@@ -16,14 +17,32 @@ printProfileData(profileDataArgs); */
 const promptUser = () => {
     return inquirer.prompt([
         {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name?'
+            type: 'input',
+            name: 'name',
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                }
+                else{
+                    console.log('Please enter your name.');
+                    return false;
+                };
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username'
+            message: 'Enter your GitHub Username (Required)',
+            validate: githubInput => {
+                if(githubInput) {
+                    return true;
+                }
+                else{
+                    console.log('Please enter your username.');
+                    return false;
+                };
+            }
         },
         {
             type: 'input',
@@ -47,12 +66,30 @@ const promptProject = portfolioData => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
+        message: 'What is the name of your project? (Required)',
+        validate: nameInput => {
+            if(nameInput) {
+                return true;
+            }
+            else{
+                console.log('Please enter your name.');
+                return false;
+            };
+        }
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)'
+        message: 'Provide a description of the project (Required)',
+        validate: descInput => {
+            if(descInput) {
+                return true;
+            }
+            else{
+                console.log('Please enter your name.');
+                return false;
+            };
+        }
       },
       {
         type: 'checkbox',
@@ -63,7 +100,16 @@ const promptProject = portfolioData => {
       {
         type: 'input',
         name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)'
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: linkInput => {
+            if(linkInput) {
+                return true;
+            }
+            else{
+                console.log('Please enter your name.');
+                return false;
+            };
+        }
       },
       {
         type: 'confirm',
